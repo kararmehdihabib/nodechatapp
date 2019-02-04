d4513b80dec66cbc42379953829699ca1e61f415
KARAR MEHDI HABIB <karar.habib@castor.fi>
2019-02-04 13:54:04 +0200

    new user and alert others

--- server/server.js
+++ server/server.js
@@ -14,6 +14,23 @@ app.use(express.static(publicPath));
 
 io.on('connection', (socket) => {
   console.log('new user connected.');
+   
+   socket.emit('newMessage', {
+   	from: 'Admin',
+   	text: "Welcome to chat app",
+   	createdAt: new Date().getTime()
+
+   });
+
+
+    socket.broadcast.emit('newMessage', {
+   	from: 'Admin',
+   	text: "New user joined",
+   	createdAt: new Date().getTime()
+   });
+  
+  
+
 
   socket.on('createMessage', (message) => {
   	console.log('createMessage', message);
@@ -22,6 +39,11 @@ io.on('connection', (socket) => {
   		text: message.text,
   		createdAt: new Date().getTime()
   	});
+  	/*socket.broadcast.emit('newMessage',{
+  		from: message.from,
+  		text: message.text,
+  		createdAt: new Date().getTime()
+  	});*/
   		  });
 
   socket.on('disconnect', () => {
