055abc31d716ed0d65e030c95bd269025b563be4
KARAR MEHDI HABIB <karar.habib@castor.fi>
2019-02-04 12:52:42 +0200

    Add new message and create message events

---
+++ public/js/index.js
@@ -0,0 +1,19 @@
+var socket = io();
+
+
+	socket.on('connect', function() {
+		console.log('connected to server');
+
+		socket.emit('createMessage', {
+			from: 'Karar',
+			text: "Thats"
+		});
+	});
+
+	socket.on('disconnect', function(){
+		console.log('disconnected from server');
+	});
+
+	socket.on('newMessage', function(message){
+		console.log('newMessage', message);
+	});
