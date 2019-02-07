6ad3db330dba7e1e694b3ac4fe091900984964cd
KARAR MEHDI HABIB <karar.habib@castor.fi>
2019-02-07 14:26:50 +0200

    Readme added

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,109 +1,109 @@
-var socket = io();
+	var socket = io();
 
 
-function scrollToBottom(){
-	var messages = jQuery('#messages');
-	var newMessage = messages.children('li:last-child');
-	var clientHeight = messages.prop('clientHeight');
-	var scrollTop = messages.prop('scrollTop');
-	var scrollHeight = messages.prop('scrollHeight');
-	var newMessageHeight = newMessage.innerHeight();
-	var lastMessageHeight = newMessage.prev().innerHeight();
+	function scrollToBottom(){
+		var messages = jQuery('#messages');
+		var newMessage = messages.children('li:last-child');
+		var clientHeight = messages.prop('clientHeight');
+		var scrollTop = messages.prop('scrollTop');
+		var scrollHeight = messages.prop('scrollHeight');
+		var newMessageHeight = newMessage.innerHeight();
+		var lastMessageHeight = newMessage.prev().innerHeight();
 
-	if(clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight){
-			messages.scrollTop(scrollHeight);
+		if(clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight){
+				messages.scrollTop(scrollHeight);
+		}
 	}
-}
 
-	socket.on('connect', function() {
-		
-		var params = jQuery.deparam(window.location.search);
-		socket.emit('join', params, function(err){
-			if (err){
-				alert(err);
-				window.location.href = '/';
-			}else{
-				
-			}
+		socket.on('connect', function() {
+			
+			var params = jQuery.deparam(window.location.search);
+			socket.emit('join', params, function(err){
+				if (err){
+					alert(err);
+					window.location.href = '/';
+				}else{
+					
+				}
+			});
+
+
+			
 		});
 
+		socket.on('disconnect', function(){
+			console.log('disconnected from server');
+		});
 
-		
+		socket.on('updateUserList', function(users){
+	var ol = jQuery('<ol></ol>'); 
+
+	users.forEach(function (user) {
+		ol.append(jQuery('<li></li>').text(user));
 	});
 
-	socket.on('disconnect', function(){
-		console.log('disconnected from server');
+	jQuery('#users').html(ol);
+
 	});
 
-	socket.on('updateUserList', function(users){
-var ol = jQuery('<ol></ol>'); 
+		socket.on('newMessage', function(message){
+			var formattedTime = moment(message.createdAt).format('h:mm a');
+			var template = jQuery('#message-template').html();
+			var html = Mustache.render(template, {
+				text: message.text,
+				from: message.from,
+				createdAt: formattedTime
+			});
 
-users.forEach(function (user) {
-	ol.append(jQuery('<li></li>').text(user));
-});
+			jQuery('#messages').append(html);
+			scrollToBottom();
 
-jQuery('#users').html(ol);
+			
+		});
 
-});
-
-	socket.on('newMessage', function(message){
+	socket.on('newLocationMessage', function(message){
 		var formattedTime = moment(message.createdAt).format('h:mm a');
-		var template = jQuery('#message-template').html();
-		var html = Mustache.render(template, {
-			text: message.text,
+		var template = jQuery('#location-message-template').html();
+		var html =  Mustache.render(template, {
 			from: message.from,
+			url: message.url,
 			createdAt: formattedTime
 		});
 
 		jQuery('#messages').append(html);
 		scrollToBottom();
-
 		
 	});
 
-socket.on('newLocationMessage', function(message){
-	var formattedTime = moment(message.createdAt).format('h:mm a');
-	var template = jQuery('#location-message-template').html();
-	var html =  Mustache.render(template, {
-		from: message.from,
-		url: message.url,
-		createdAt: formattedTime
-	});
+		
 
-	jQuery('#messages').append(html);
-	scrollToBottom();
-	
-});
-
-	
-
-	jQuery('#message-form').on('submit', function(e){
-		e.preventDefault();
-		var meesageTextBox = jQuery('[name=message]');
-		socket.emit('createMessage', {
-			text: jQuery('[name=message]').val()
-		}, function(){
-			meesageTextBox.val('');
+		jQuery('#message-form').on('submit', function(e){
+			e.preventDefault();
+			var meesageTextBox = jQuery('[name=message]');
+				socket.emit('createMessage', {
+				text: jQuery('[name=message]').val()
+			}, function(){
+				meesageTextBox.val('');
+			});
 		});
-	});
 
-	var locationButton = jQuery('#send-location');
+		var locationButton = jQuery('#send-location');
 
-	locationButton.on('click', function(){
-		if(!navigator.geolocation){
-			return alert('Geolocation not supported by your browser.');
-		}
+		locationButton.on('click', function(){
+			if(!navigator.geolocation){
+				return alert('Geolocation not supported by your browser.');
+			}
 
-		locationButton.attr('disabled', 'disabled').text('Sending location...');
+			locationButton.attr('disabled', 'disabled').text('Sending location...');
 
-		navigator.geolocation.getCurrentPosition(function(position){
-			locationButton.removeAttr('disabled').text('Send location');;
-				socket.emit('createLocationMessage',{
-					latitude: position.coords.latitude,
-					longitude: position.coords.longitude
-				});
-		}, function(){
-			locationButton.removeAttr('disabled').text('Send location');
-			alert('Unable to fetch location.');
+			navigator.geolocation.getCurrentPosition(function(position){
+				locationButton.removeAttr('disabled').text('Send location');;
+					socket.emit('createLocationMessage',{
+						latitude: position.coords.latitude,
+						longitude: position.coords.longitude
+					});
+			}, function(){
+				locationButton.removeAttr('disabled').text('Send location');
+				alert('Unable to fetch location.');
+			});
 		});
-	});
--- server/server.js
+++ server/server.js
@@ -4,10 +4,10 @@ const express = require('express');
 const socketIO = require('socket.io');
 
 
-const {generateMessage, generateLocationMessage} = require('./utils/message');
-const {isRealString} = require('./utils/validation');
+const { generateMessage, generateLocationMessage } = require('./utils/message');
+const { isRealString } = require('./utils/validation');
 const publicPath = path.join(__dirname, '../public');
-const {Users} = require('./utils/users');
+const { Users } = require('./utils/users');
 
 var app = express();
 var server = http.createServer(app);
@@ -18,12 +18,9 @@ var users = new Users();
 app.use(express.static(publicPath));
 
 io.on('connection', (socket) => {
-  console.log('new user connected.');
-   
-  
-  
+
   socket.on('join', (params, callback) => {
-    if(!isRealString(params.name) || !isRealString(params.room)){
+    if (!isRealString(params.name) || !isRealString(params.room)) {
       return callback('Name and room name are required.');
     }
 
@@ -34,67 +31,53 @@ io.on('connection', (socket) => {
 
     io.to(params.room).emit('updateUserList', users.getUserList(params.room));
 
-     socket.emit('newMessage', generateMessage('Admin', 'Welcome to CAPP'));
+    socket.emit('newMessage', generateMessage('Admin', 'Welcome to CAPP'));
 
 
     socket.broadcast.to(params.room).emit('newMessage', generateMessage('Admin', `${params.name} has joined.`));
-    
+
 
     callback();
   });
 
 
   socket.on('createMessage', (message, callback) => {
-  	var user = users.getUser(socket.id);
+    var user = users.getUser(socket.id);
 
 
-    if(user && isRealString(message.text)) {
+    if (user && isRealString(message.text)) {
       io.to(user.room).emit('newMessage', generateMessage(user.name, message.text));
     }
-  	
- 	 
- 	 callback();
 
-  		  });
+
+    callback();
+
+  });
 
   socket.on('createLocationMessage', (coords) => {
 
     var user = users.getUser(socket.id);
 
 
-    if(user) {
+    if (user) {
       io.to(user.room).emit('newLocationMessage', generateLocationMessage(user.name, coords.latitude, coords.longitude));
     }
-    
+
   });
 
 
   socket.on('disconnect', () => {
-		var user = users.removeUser(socket.id);
+    var user = users.removeUser(socket.id);
 
 
-    if(user) {
+    if (user) {
       io.to(user.room).emit('updateUserList', users.getUserList(user.room));
       io.to(user.room).emit('newMessage', generateMessage('Admin', `${user.name} has left.`));
     }
-	});
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  });
 
 });
 
 server.listen(port, () => {
-	console.log('Server is up on port ' + port);
+  console.log('Server is up on port ' + port);
 });
